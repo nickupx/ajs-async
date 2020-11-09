ed75330fb371c2b5dd474a813c28653bb2de93ca
nickupx <nick.upsky@gmail.com>
2020-11-09 22:48:22 +0200

    fixes

--- src/js/__tests__/gamesavingloader.test.js
+++ src/js/__tests__/gamesavingloader.test.js
@@ -1,7 +1,8 @@
 import GameSavingLoader from '../gamesavingloader'
 
+const expected = '{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}'
+
 test('should return', async () => {
-  const expected = '{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}'
   const received = await GameSavingLoader.load()
   expect(received).toEqual(expected)
 })
