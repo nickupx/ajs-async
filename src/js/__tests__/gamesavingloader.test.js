3991e300a85578b40666d2673691d14984436f64
nickupx <nick.upsky@gmail.com>
2020-11-09 22:43:15 +0200

    2 done but test?

--- src/js/__tests__/gamesavingloader.test.js
+++ src/js/__tests__/gamesavingloader.test.js
@@ -1,9 +1,7 @@
 import GameSavingLoader from '../gamesavingloader'
 
-const expected = '{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}'
-
-test('should return', () => {
-  GameSavingLoader.load().then((received) => {
-    expect(received).toEqual(expected);
-  }).catch((error) => error);
-});
+test('should return', async () => {
+  const expected = '{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}'
+  const received = await GameSavingLoader.load()
+  expect(received).toEqual(expected)
+})
--- src/js/gamesavingloader.js
+++ src/js/gamesavingloader.js
@@ -2,9 +2,14 @@ import read from './parser'
 import json from './reader'
 
 export default class GameSavingLoader {
-  static load() {
-    // const data = read(); // возвращается Promise!
-    // const value = json(data); // возвращается Promise!
-    return read().then((data) => json(data))
+  static async load() {
+    let result
+    try {
+      const data = await read()
+      result = await json(data)
+    } catch (e) {
+      return e
+    }
+    return result
   }
 }
